82b1d476082e9248d7930126ead543a6bafb92ec
Animesh Kahara <animeshcyberphysco@gmail.com>
2019-03-04 00:16:47 +0530

    Initial Commit

---
+++ featureXtractor.py
@@ -0,0 +1,8 @@
+def main():
+    print("\t\tfeatureXtractor VER:0.0")
+    print("----------------------------------------------------------------------")
+    print("\n\nHi!! I am ver 0.1");
+    
+
+if __name__=='__main()__':
+    main()
