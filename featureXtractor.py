3f114070f437e54cf08adea4b6eaa16981e28822
Animesh Kahara <animeshcyberphysco@gmail.com>
2019-03-04 00:33:56 +0530

    -Url Decoded and page-content displayed

--- featureXtractor.py
+++ featureXtractor.py
@@ -1,8 +1,15 @@
+import urllib.request
+
+
 def main():
-    print("\t\tfeatureXtractor VER:0.0")
+    print("\t\tfeatureXtractor VER:0.1")
     print("----------------------------------------------------------------------")
     print("\n\nHi!! I am ver 0.1");
+    content = urllib.request.urlopen('http://www.python.org/')
+    for l in content:
+        print(l)
     
 
-if __name__=='__main()__':
-    main()
+#if __name__=='__main()__':
+#    main()
+main()
